ae3c5338e6bd850b47ec41e670b9e8a8ecfdbe80
Adriano Vitor <adrianofelix019@gmail.com>
2025-04-11 16:34:00 -0300

    initial commit

---
+++ renderer.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const botao = document.getElementById('meu-botao');
+    botao.addEventListener('click', () => {
+        alert('Você clicou no botão!');
+    });
+});
