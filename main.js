039fd739b56d6ea820abb60b92e11c9b8b38a240
Adriano Vitor <adrianofelix019@gmail.com>
2025-04-18 09:43:46 -0300

    style: improve hover effects and add styled button

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function createWindow() {
     height: 600,
     webPreferences: {
       contextIsolation: true,
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, "preload.js"),
       nodeIntegration: false,
       additionalArguments: [`--soundPath=${path.join(__dirname, "assets", "sounds")}`],
     },
